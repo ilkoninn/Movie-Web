4280e0c790a233af0d84cd5cf57f935850dcf1f7
Recebov Ilkin <recebovilkin256@gmail.com>
2023-11-10 17:27:02 +0400

    LimFlix created, v1.0

--- js/scriptToGet.js
+++ js/scriptToGet.js
@@ -1,23 +1,12 @@
-let shows = JSON.parse(localStorage.getItem("shows"));
-
-shows = [];
-axios.get("https://api.tvmaze.com/shows").then((res) => {
-  let temp = 0;
-  let tempShow = {};
-
-  for (let i = 0; i < 236; i++) {
-    for (let j = 0; j < res.data.length; j++) {
-      if (
-        res.data[j].rating.average > temp &&
-        !shows.find((show) => show.id === res.data[j].id)
-      ) {
-        temp = res.data[j].rating.average;
-        tempShow = res.data[j];
-      }
+async function fetchShows() {
+    if (!shows || shows.length === 0) {
+        await axios.get('https://api.tvmaze.com/shows')
+            .then(res => {
+                console.log("I am fetching shows");
+                shows = res.data;
+                localStorage.setItem('shows', JSON.stringify(shows));
+            });
     }
-    shows.push(tempShow);
-    temp = 0;
-  }
+}
 
-  localStorage.setItem("shows", JSON.stringify(shows));
-});
+fetchShows();
